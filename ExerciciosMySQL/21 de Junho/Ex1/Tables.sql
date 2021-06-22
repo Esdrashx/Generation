3e080498e9cdf4f87cc94ac8660143acacadc044
Esdrashx <70281835+Esdrashx@users.noreply.github.com>
2021-06-22 14:27:31 -0300

    Create Tables.sql

---
+++ ExerciciosMySQL/21 de Junho/Ex1/Tables.sql
@@ -0,0 +1,22 @@
+CREATE  DATABASE  db_generation_game_online ;
+USE db_generation_game_online;
+
+CRIAR  TABELA  tabela_classe (
+id_classe INT  NOT NULL AUTO_INCREMENT,
+carreira VARCHAR ( 255 ) NOT NULL,
+sexo VARCHAR ( 255 ) NOT NULL,
+estilo VARCHAR ( 255 ) NOT NULL,
+CHAVE PRIMÁRIA (id_classe)
+);
+
+CRIAR  TABELA  tabela_personagem (
+id_personagem INT  NOT NULL AUTO_INCREMENT,
+nome VARCHAR ( 255 ) NOT NULL,
+poder_ataque INT  NOT NULL,
+poder_defesa INT  NOT NULL,
+vida INT  NOT NULL,
+habilidade VARCHAR ( 255 ) NOT NULL,
+fk_classe INT,
+PRIMARY KEY (id_personagem),
+CHAVE ESTRANGEIRA (fk_classe) REFERÊNCIAS tabela_classe (id_classe)
+);
