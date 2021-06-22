01766ecacd28cc04db10bf49d159580e1f0ad93c
Esdrashx <70281835+Esdrashx@users.noreply.github.com>
2021-06-22 14:30:30 -0300

    Create Selects.sql

---
+++ ExerciciosMySQL/21 de Junho/Ex1/Selects.sql
@@ -0,0 +1,20 @@
+USE db_generation_game_online;
+
+SELECT nome, poder_ataque FROM tabela_personagem 
+WHERE poder_ataque > 2000;
+
+SELECT nome, poder_defesa FROM tabela_personagem 
+WHERE poder_defesa >= 1000 AND poder_defesa <= 2000;
+
+SELECT nome "Nome:" FROM tabela_personagem 
+WHERE nome LIKE 'C%';
+    
+SELECT * FROM tabela_classe
+INNER JOIN tabela_personagem ON tabela_personagem.fk_classe = tabela_classe.id_classe
+ORDER BY nome ASC;
+
+SELECT tabela_personagem.nome "Nome:", tabela_classe.carreira "Carreira:" 
+FROM tabela_personagem
+INNER JOIN tabela_classe ON tabela_classe.id_classe = tabela_personagem.fk_classe
+WHERE tabela_classe.carreira = "Arqueiro"
+ORDER BY tabela_personagem.nome ASC;
