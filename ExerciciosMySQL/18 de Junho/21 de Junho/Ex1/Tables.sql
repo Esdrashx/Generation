d8929df3759b96b643e78cc393f02ffc5b01d09f
Esdrashx <70281835+Esdrashx@users.noreply.github.com>
2021-06-21 22:55:59 -0300

    Update Tables.sql

--- ExerciciosMySQL/18 de Junho/21 de Junho/Ex1/Tables.sql
+++ ExerciciosMySQL/18 de Junho/21 de Junho/Ex1/Tables.sql
@@ -1,23 +1,24 @@
-CREATE DATABASE db_generation_game_online;
+CREATE  DATABASE  db_generation_game_online ;
 USE db_generation_game_online;
 
-CREATE TABLE tabela_classe(
-	id_classe INT NOT NULL AUTO_INCREMENT,
-	carreira VARCHAR(255) NOT NULL,
-    sexo VARCHAR(255) NOT NULL,
-    estilo VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id_classe)
+CRIAR  TABELA  tabela_classe (
+	id_classe INT  NOT NULL AUTO_INCREMENT,
+	carreira VARCHAR ( 255 ) NÃO NULO ,
+    sexo VARCHAR ( 255 ) NÃO NULO ,
+    estilo VARCHAR ( 255 ) NÃO NULO ,
+    CHAVE PRIMÁRIA (id_classe)
 );
 
-CREATE TABLE tabela_personagem(
-	id_personagem INT NOT NULL AUTO_INCREMENT,
-    nome VARCHAR(255) NOT NULL,
-	poder_ataque INT NOT NULL,
-    poder_defesa INT NOT NULL,
-    vida INT NOT NULL,
-    habilidade VARCHAR(255) NOT NULL,
-    cp_classe INT,
-    PRIMARY KEY(id_personagem),
-    FOREIGN KEY(cp_classe) REFERENCES tb_classe(id_classe)
+CRIAR  TABELA  tabela_personagem (
+	id_personagem INT  NOT NULL AUTO_INCREMENT,
+    nome VARCHAR ( 255 ) NÃO NULO ,
+	poder_ataque INT  NOT NULL ,
+    poder_defesa INT  NOT NULL ,
+    vida INT  NOT NULL ,
+    habilidade VARCHAR ( 255 ) NÃO NULO ,
+    cp_classe INT ,
+    PRIMARY KEY (id_personagem),
+    CHAVE ESTRANGEIRA (cp_classe) REFERÊNCIAS tabela_classe (id_classe)
 );
+
 
