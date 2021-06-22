867171c060914b2c9f61a68797fdd46723ab0ef0
Esdrashx <70281835+Esdrashx@users.noreply.github.com>
2021-06-21 23:36:13 -0300

    Update Tables.sql

--- ExerciciosMySQL/18 de Junho/21 de Junho/Ex1/Ex2/Tables.sql
+++ ExerciciosMySQL/18 de Junho/21 de Junho/Ex1/Ex2/Tables.sql
@@ -1,22 +1,20 @@
-
-CREATE  DATABASE  db_pizzaria_legal ;
+CREATE DATABASE db_pizzaria_legal;
 USE db_pizzaria_legal;
 
-CRIAR  TABELA  tabela_categoria (
-	id_categoria INT  NOT NULL AUTO_INCREMENT,
-    montar_pizza VARCHAR ( 255 ) NÃO NULO ,
-    promocao int  not null ,
-	CHAVE PRIMÁRIA (id_categoria)
+CREATE TABLE tabela_categoria(
+	id_categoria INT NOT NULL AUTO_INCREMENT,
+    montar_pizza VARCHAR(255) NOT NULL,
+    quantidade INT NOT NULL,
+	PRIMARY KEY(id_categoria)
 );
 
-CRIAR  TABELA  tabela_pizza (
-	id_pizza INT  NOT NULL AUTO_INCREMENT,
-    nome_pizza VARCHAR ( 255 ) NÃO NULO ,
-    preco FLOAT NOT NULL ,
-    quantidade int  não nula ,
-    estoque BOOLEANO  NÃO NULO ,
-    validação DATA  NÃO NULO ,
-    cp_categoria INT  NOT NULL ,
+CREATE TABLE tabela_pizza(
+	id_pizza INT NOT NULL AUTO_INCREMENT,
+    nome_pizza VARCHAR(255) NOT NULL,
+    preco FLOAT NOT NULL,
+    estoque BOOLEAN NOT NULL,
+    validade DATE NOT NULL,
+    cp_categoria INT NOT NULL,
     PRIMARY KEY (id_pizza),
-    CHAVE ESTRANGEIRA (cp_categoria) REFERÊNCIAS tabela_categoria (id_categoria)
+    FOREIGN KEY(cp_categoria) REFERENCES tabela_categoria(id_categoria)
 );
