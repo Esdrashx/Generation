69fc5cc9526f0d61239d79c11258feda51c3b368
Esdrashx <70281835+Esdrashx@users.noreply.github.com>
2021-06-22 13:51:08 -0300

    Update Tables.sql

--- ExerciciosMySQL/18 de Junho/21 de Junho/Ex1/Tables.sql
+++ ExerciciosMySQL/18 de Junho/21 de Junho/Ex1/Tables.sql
@@ -3,22 +3,22 @@ USE db_generation_game_online;
 
 CRIAR  TABELA  tabela_classe (
 	id_classe INT  NOT NULL AUTO_INCREMENT,
-	carreira VARCHAR ( 255 ) NÃO NULO ,
-    sexo VARCHAR ( 255 ) NÃO NULO ,
-    estilo VARCHAR ( 255 ) NÃO NULO ,
+	carreira VARCHAR ( 255 ) NOT NULL,
+    sexo VARCHAR ( 255 ) NOT NULL,
+    estilo VARCHAR ( 255 ) NOT NULL,
     CHAVE PRIMÁRIA (id_classe)
 );
 
 CRIAR  TABELA  tabela_personagem (
 	id_personagem INT  NOT NULL AUTO_INCREMENT,
-    nome VARCHAR ( 255 ) NÃO NULO ,
-	poder_ataque INT  NOT NULL ,
-    poder_defesa INT  NOT NULL ,
-    vida INT  NOT NULL ,
-    habilidade VARCHAR ( 255 ) NÃO NULO ,
-    cp_classe INT ,
+    nome VARCHAR ( 255 ) NOT NULL,
+	poder_ataque INT  NOT NULL,
+    poder_defesa INT  NOT NULL,
+    vida INT  NOT NULL,
+    habilidade VARCHAR ( 255 ) NOT NULL,
+    fk_classe INT,
     PRIMARY KEY (id_personagem),
-    CHAVE ESTRANGEIRA (cp_classe) REFERÊNCIAS tabela_classe (id_classe)
+    CHAVE ESTRANGEIRA (fk_classe) REFERÊNCIAS tabela_classe (id_classe)
 );
 
 
